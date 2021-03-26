afce752b8c34f7688c3116b40a610982a4da6a3e
B3Funky <tyutin.s@list.ru>
2021-03-27 01:58:09 +0300

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: '',
+    getLength() {
+        return this.chain.split('(').length - 1;
+    },
+    addLink(value) {
+        if (value === undefined) {
+            value = ''
+        }
+        this.chain += `( ${value} )~~`;
+        return this
+    },
+    removeLink(position) {
+        if (typeof position === 'number' && position > 0 && position <= this.getLength() && !(position % 1)) {
+            let newChain = this.chain.split('( ');
+            newChain.splice(position, 1);
+            this.chain = newChain.join('( ');
+        } else {
+            this.chain = '';
+            throw Error
+        }
+        return this
+    },
+    reverseChain() {
+        let newChain = this.chain.split('( ');
+        for (let i = 0; i < newChain.length; i++) {
+            if (newChain[i].slice(-4) === ' )~~') {
+                newChain[i] = newChain[i].slice(0, newChain[i].length - 4);
+            }
+            if (newChain[i].slice(-2) === ' )') {
+                newChain[i] = newChain[i].slice(0, newChain[i].length - 2);
+            }
+        }
+        newChain.reverse();
+        newChain.pop();
+        this.chain = '';
+        for (let i = 0; i < newChain.length; i++) {
+            this.addLink(newChain[i])
+        }
+        return this
+    },
+    finishChain() {
+        let finishChain = this.chain.slice(0, -2);
+        this.chain = '';
+        return finishChain
+    }
 };
 
 module.exports = chainMaker;
