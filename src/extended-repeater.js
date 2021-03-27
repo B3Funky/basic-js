630fc721238523b8593a841bb2dff36cb04bec08
B3Funky <tyutin.s@list.ru>
2021-03-27 07:31:03 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    if (str === undefined) {
+        throw Error
+    }
+    if (str !== null && typeof str !== 'string' && typeof str !== 'object') {
+        str = str.toString();
+    }
+    if (!options.repeatTimes) {
+        options.repeatTimes = 1
+    }
+    if (!options.separator) {
+        options.separator = '+'
+    }
+    if (options.addition !== null) {
+        if (options.addition !== false && !options.addition) {
+            options.addition = ''
+        } else if (typeof options.addition !== 'string' && typeof str !== 'object') {
+            options.addition = options.addition.toString();
+        }
+    }
+    if (!options.additionRepeatTimes) {
+        options.additionRepeatTimes = 1
+    }
+    if (!options.additionSeparator) {
+        options.additionSeparator = '|'
+    }
+
+    let string = '';
+    let addition = '';
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+        addition += options.addition;
+        if (i < options.additionRepeatTimes - 1) {
+            addition += options.additionSeparator
+        }
+    }
+    for (let j = 0; j < options.repeatTimes; j++) {
+        string += str + addition;
+        if (j < options.repeatTimes - 1) {
+            string += options.separator
+        }
+    }
+
+    return string
 };
-  
